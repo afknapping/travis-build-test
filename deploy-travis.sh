0177de7e70948563652de1b9e1ef91c8d2fd7db7
filtercake <fabian@superpursuitmo.de>
2016-03-06 11:26:45 +0100

    more testing

--- deploy-travis.sh
+++ deploy-travis.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# expects GH_REF github.com/username/repo
+# expects GH_TOKEN
 
 cd _build
 git init
@@ -7,6 +9,4 @@ git config user.name "Travis CI"
 git config user.email "<your@email.com>"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-
-# > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
